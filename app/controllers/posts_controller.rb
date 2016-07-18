b8afebc3c8a476725abe174d6bde6425c1aef4bf
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-18 21:38:06 +0200

    update method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,13 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    end
+  end
+
   def new
     @post = Post.new
   end
