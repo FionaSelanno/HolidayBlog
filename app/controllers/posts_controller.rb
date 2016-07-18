b830d734c06287ae87fe40afba04e4a8faaa02ff
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-18 22:09:19 +0200

    replaced destroy method and added delete link showpage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   def new
     @post = Post.new
   end
@@ -26,12 +32,6 @@ class PostsController < ApplicationController
     @post = Post.new( post_params )
     if @post.save
       redirect_to @post
-  end
-
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to posts_path
   end
 end
 
