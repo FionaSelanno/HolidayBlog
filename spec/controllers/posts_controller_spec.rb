8edc8a348398fb26967dbb5b2712f11ec5782ebb
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-19 12:29:47 +0200

    test controller GET#show

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -21,7 +21,18 @@ describe PostsController do
 #   describe "GET #show" do
 #     it "assigns the requested post as @post"
 #   end
+
+  describe "GET #show" do
+    it "assigns @post and renders the show template" do
+      post = Post.create(title: "A summer on Ibiza", content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.")
+      get :show, id: post.id
+
+      expect(assigns(:post)).to eq(post)
+      expect(response).to render_template("show")
+    end
+  end
 #
+
 #   describe "GET #new" do
 #     it "assigns a new post as @post"
 #   end
