bc5691ab4f731e803b040aa838194f50b9413acc
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-07-19 13:47:29 +0200

    test controller GET#edit

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 # Anatomy of a controller spec:
 
 describe PostsController do
+
   # describe "GET #index" do
   #   it "gets index"
   #   it "assigns posts variable"
@@ -49,6 +50,18 @@ describe PostsController do
 #     it "assigns the requested post as @post"
 #     it "renders the edit template"
 #   end
+  describe "GET #edit" do
+    it "assigns the requested post as @post
+        and renders the edit template" do
+      post = Post.create(title: "A summer on Ibiza", content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.")
+
+      get :edit, id: post.id
+
+      expect(assigns(:post)).to eq(post)
+      expect(response).to render_template("edit")
+    end
+  end
+
 #
 #   describe "POST #create" do
 #     context "with valid params"
